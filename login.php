fb89dfd90c3c9e7ad030d3b371406b52d5771bac
unknown <yopannalpiss@gmail.com>
2023-11-11 22:42:46 +0700

    FE reg/log

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+include("inc/link.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="logBox">
+        <div class="logTop">
+            <div>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <h2>Register</h2>
+        </div>
+        <div class="logBot">
+            <form action="" method="post">
+                <div>
+                    <label for="username">Username :</label>
+                    <div class="inputBox">
+                        <i class="fa-solid fa-user"></i>
+                        <input type="text" name="username">
+                    </div>
+                    <label for="password">Password :</label>
+                    <div class="inputBox">
+                        <i class="fa-solid fa-key"></i>
+                        <input type="password" name="password" id="">
+                    </div>
+                </div>
+                <input class="submit" type="submit" name="submit" value="Daftar">
+            </form>
+            <p>Belum punya akun?<a href="register.php">Register di sini!</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,48 @@
+<?php
+include("inc/link.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+
+<body>
+    <div class="logBox">
+        <div class="logTop">
+            <div>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <h2>Register</h2>
+        </div>
+        <div class="logBot">
+            <form action="" method="post">
+                <div>
+                    <label for="username">Username :</label>
+                    <div class="inputBox">
+                        <i class="fa-solid fa-user"></i>
+                        <input type="text" name="username">
+                    </div>
+                    <label for="password">Password :</label>
+                    <div class="inputBox">
+                        <i class="fa-solid fa-key"></i>
+                        <input type="password" name="password" id="">
+                    </div>
+                    <label for="confPassword">Confirm Password :</label>
+                    <div class="inputBox">
+                        <i class="fa-solid fa-key"></i>
+                        <input type="password" name="confPassword" id="">
+                    </div>
+                </div>
+                <input class="submit" type="submit" name="submit" value="Daftar">
+            </form>
+            <p>Sudah punya akun?<a href="login.php">Login di sini!</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
