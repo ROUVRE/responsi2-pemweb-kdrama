c297347bc2bed9c0b850e6228750141752dd1aa0
ROUVRE <reynoalfarez04@gmail.com>
2023-11-19 20:34:11 +0700

    Updated database
    tabel baru

--- database.sql
+++ database.sql
@@ -14,5 +14,45 @@ CREATE TABLE akun (
 CREATE TABLE film (
     id_film INT AUTO_INCREMENT PRIMARY KEY,
     judul VARCHAR(255) NOT NULL,
+    sutradara VARCHAR(40) NOT NULL,
+    tahun_rilis YEAR NOT NULL,
     rating INT CHECK (rating >= 0 AND rating <= 10)
+);
+
+CREATE TABLE aktor (
+    id_aktor INT PRIMARY KEY,
+    nama_aktor VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE film_aktor (
+    id_film INT,
+    id_aktor INT,
+    PRIMARY KEY (id_film, id_aktor),
+    FOREIGN KEY (id_film) REFERENCES film(id_film),
+    FOREIGN KEY (id_aktor) REFERENCES aktor(id_aktor)
+);
+
+CREATE TABLE review (
+    id_review INT AUTO_INCREMENT PRIMARY KEY,
+    id_film INT,
+    user VARCHAR(255) NOT NULL,
+    nilai INT CHECK (nilai >= 0 AND nilai <= 10),
+    komentar TEXT,
+    waktu_review TIMESTAMP,
+    FOREIGN KEY (id_film) REFERENCES film(id_film)
+);
+
+CREATE TABLE list_kustom (
+    id_list INT PRIMARY KEY NOT NULL,
+    id_akun INT,
+    judul_list VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_akun) REFERENCES akun(id_akun)
+);
+
+CREATE TABLE film_dalam_list (
+    id_listed_film INT PRIMARY KEY,
+    id_list INT,
+    id_film INT,
+    FOREIGN KEY (id_list) REFERENCES list_kustom(id_list),
+    FOREIGN KEY (id_film) REFERENCES film(id_film)
 );
