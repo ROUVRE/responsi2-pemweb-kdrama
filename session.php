56572e5ca8becacd2b5729cfa57a2424cad4c8a0
ROUVRE <reynoalfarez04@gmail.com>
2023-11-25 09:17:46 +0700

    Updated session
    added new function buat nyimpen data login ke session

--- session.php
+++ session.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+function set_login_session($user_id, $nama, $username, $role) {
+    $_SESSION['user_id'] = $user_id;
+    $_SESSION['nama'] = $nama;
+    $_SESSION['username'] = $username;
+    $_SESSION['role'] = $role;
+}
 function check_session() {
     if (!isset($_SESSION['username'])) {
         header("Location: login.php");
