fce351f8a08b982eb9271dc3af5b543feadb5700
ROUVRE <reynoalfarez04@gmail.com>
2023-11-26 15:22:43 +0700

    Updated session (lagi)
    Role based access (admin bisa akses semua file, user gk bisa akses laman admin)

--- session.php
+++ session.php
@@ -8,8 +8,34 @@ function set_login_session($user_id, $nama, $username, $role) {
 }
 function check_session() {
     if (!isset($_SESSION['username'])) {
-        header("Location: login.php");
-        exit();
+        redirect_to_login();
+    } else {
+        $role = $_SESSION['role'];
+        
+        if ($role === 'admin') {
+            //Kalau akun admin, boleh masuk ke semua file
+            return;
+        } elseif ($role === 'user') {
+            //Kalau akun user, bakal ngelempar mereka ke index.php (khusus user) kalau mereka nyobain buat akses file di subfolder 'admin'
+            $main_dir = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+            if (strpos($main_dir, '/admin') !== false) {
+                header("Location: $main_dir/../index.php");
+                exit();
+            }
+        }
     }
 }
+
+function redirect_to_login() {
+    $main_dir = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+    if (strpos($main_dir, '/admin') !== false) {
+        header("Location: $main_dir/../login.php");
+    } else {
+        header("Location: $main_dir/login.php");
+    }
+    exit();
+}
+
 ?>
